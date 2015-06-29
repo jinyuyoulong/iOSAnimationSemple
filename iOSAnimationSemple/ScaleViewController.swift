fc7202dced6b1210cd7da1b0f174670793917aa6
jinyuyoulong <fan.jinlong@qq.com>
2015-06-29 15:45:37 +0800

    scale

---
+++ iOSAnimationSemple/ScaleViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ScaleViewController.swift
+//  iOSAnimationSemple
+//
+//  Created by fans on 15/6/19.
+//  Copyright (c) 2015年 fans. All rights reserved.
+//
+
+import UIKit
+
+class ScaleViewController: UIViewController {
+
+    @IBOutlet weak var blueSquare: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animateWithDuration(1, animations: {
+        self.blueSquare.transform =   CGAffineTransformMakeScale(0.3, 2.0)
+        
+        })
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
